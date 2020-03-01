f42c21e223cfb5ffd4d2496b1aaba09bb59c5bbc
Dave Vazquez <dmvaz.li@gmail.com>
2020-02-29 19:27:55 -0500

    exports actions/reducers

--- src/store/index.js
+++ src/store/index.js
@@ -3,16 +3,19 @@
 ********************************************************/
 import { combineReducers } from 'redux';
 import activeTetrominoReducer from './reducers/activeTetrominoReducer';
+import gamePlayReducer from './reducers/gamePlayReducer';
 import matrixReducer from './reducers/matrixReducer';
 
 export default combineReducers({
   matrix: matrixReducer,
-  activeTetromino: activeTetrominoReducer
+  activeTetromino: activeTetrominoReducer,
+  game: gamePlayReducer
 });
 
 /********************************************************
 *                        ACTIONS                        *
 ********************************************************/
+
 export {
   // ACTIONS
   dropActiveTetromino,
@@ -20,3 +23,23 @@ export {
   SPAWN_TETROMINO_T,
   DROP_ACTIVE_TETROMINO
 } from './actions/matrixActions';
+
+export {
+  // ACTIONS
+  spawnNextTetromino,
+  // ACTION TYPES
+  SPAWN_NEXT_TETROMINO,
+  ROTATE_TETROMINO_LEFT,
+  ROTATE_TETROMINO_RIGHT,
+  MOVE_TETROMINO_LEFT,
+  MOVE_TETROMINO_RIGHT,
+  MOVE_TETROMINO_DOWN,
+  AUTO_DROP_TETROMINO,
+  SOFT_DROP_TETROMINO,
+  FAST_DROP_TETROMINO
+} from './actions/activeTetrominoActions';
+
+export {
+  beginTetris,
+  BEGIN_TETRIS
+} from './actions/gamePlayActions';
