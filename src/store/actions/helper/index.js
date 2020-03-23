cc91308cc5285144795b8031eda20488ff174cc3
Dave Vazquez <dmvaz.li@gmail.com>
2020-03-22 22:02:52 -0400

    updates so much

--- src/store/actions/helper/index.js
+++ src/store/actions/helper/index.js
@@ -1,33 +1,34 @@
-import { Tetromino } from '../../datatypes/types/Tetromino';
-import * as data from './data.json';
+import { Tetrad } from '../../store/datatypes/types/Tetrad';
+import * as tetradData from './data.json';
 /*
   Based on the original Tetris NES, Random Generator Algorithm:
 
-  0 - 6 represent the seven tetrominos
+  0 - 6 represent the seven tetrads
   7 represents a "re-roll"
-  if an 7 or the previous tetromino is rolled,
+  if an 7 or the previous tetrad is rolled,
   re-roll
-  otherwise return the next tetromino
+  otherwise return the next tetrad
 
   NOTE: omitted the 7 re-roll as it's arbitrary
 */
 const types = ['I', 'O', 'T', 'S', 'Z', 'J', 'L'];
 
-export const getRandomTetromino = lastType => {
+export const getRandomTetrad = lastType => {
   let nextNumber, nextType;
-  let tetrominos = deepCopy(data.default);
+  let tetrads = deepCopy(tetradData.default);
 
   do {
     nextNumber = Math.floor(Math.random() * 7);
     nextType = types[nextNumber];
   } while (nextType === lastType);
 
-  return new Tetromino(
-    tetrominos[nextType].type,
-    tetrominos[nextType].color,
-    tetrominos[nextType].coordinates,
-    tetrominos[nextType].rotationalIndex,
-    tetrominos[nextType].rotationalDistances
+  return new Tetrad(
+    tetrads[nextType].type,
+    tetrads[nextType].color,
+    tetrads[nextType].prevCoordinates,
+    tetrads[nextType].coordinates,
+    tetrads[nextType].orientation,
+    tetrads[nextType].rotationalMatrices
   );
 };
 
