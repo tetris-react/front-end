1e80962eb78827d4e5cb3c1b1c9b48d978e5863c
Dave Vazquez <dmvaz.li@gmail.com>
2020-03-22 22:01:42 -0400

    creates constants

---
+++ src/constants/index.js
@@ -0,0 +1,9 @@
+// Matrix Dimensions
+export const NUM_ROWS = 20;
+export const NUM_COLS = 10;
+
+// Tetrad Directions
+export const ROTATE = 'ROTATE';
+export const DOWN = 'DOWN';
+export const LEFT = 'LEFT';
+export const RIGHT = 'RIGHT';
