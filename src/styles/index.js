739f3801663f3d6740eeaf6369fa8b04157d1f71
Dave Vazquez <dmvaz.li@gmail.com>
2020-03-29 01:10:10 -0400

    creates styled containers

---
+++ src/styles/index.js
@@ -0,0 +1,3 @@
+export { default as PlayFieldContainer } from './components/PlayFieldContainer';
+export { default as AppContainer } from './components/AppContainer';
+export { default as Column } from './components/Column';
